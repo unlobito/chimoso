2735b959312b58a688991b4ea0d518b0cf5d6507
Henri Watson <henri@henriwatson.com>
2014-04-26 09:46:33 -0400

    happy new year

--- src/henriwatson/Chimoso/Event.php
+++ src/henriwatson/Chimoso/Event.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Copyright (c) 2013 Henri Watson
+ * Copyright (c) 2014 Henri Watson
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the "Software"),
